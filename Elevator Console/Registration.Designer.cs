98bd623844623d44cfbc9fa93ae6d07cc1761689
monkey <monkey@BB21003>
2016-12-14 14:32:33 -0600

    Changes to the registration form.

--- Elevator Console/Registration.Designer.cs
+++ Elevator Console/Registration.Designer.cs
@@ -29,133 +29,105 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.plateBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.cardBox = new System.Windows.Forms.TextBox();
+            this.dbInput = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.phoneNumBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 37);
+            this.label1.Location = new System.Drawing.Point(8, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "*name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "address";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 105);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "vehicle";
+            this.label1.Text = "First Name";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 137);
+            this.label4.Location = new System.Drawing.Point(7, 79);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.Size = new System.Drawing.Size(71, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "*license plate";
+            this.label4.Text = "License Plate";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(8, 177);
+            this.label5.Location = new System.Drawing.Point(8, 117);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.Size = new System.Drawing.Size(32, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "*Email";
+            this.label5.Text = "Email";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 222);
+            this.label6.Location = new System.Drawing.Point(6, 187);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(99, 13);
             this.label6.TabIndex = 5;
-            this.label6.Text = "*credit card number";
+            this.label6.Text = "Credit Card Number";
             // 
-            // textBox1
+            // nameBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(111, 37);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.nameBox.Location = new System.Drawing.Point(111, 12);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 20);
+            this.nameBox.TabIndex = 6;
+            this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
             // 
-            // textBox2
+            // plateBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(111, 70);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 7;
+            this.plateBox.Location = new System.Drawing.Point(111, 79);
+            this.plateBox.Name = "plateBox";
+            this.plateBox.Size = new System.Drawing.Size(100, 20);
+            this.plateBox.TabIndex = 8;
+            this.plateBox.TextChanged += new System.EventHandler(this.plateBox_TextChanged);
             // 
-            // textBox3
+            // emailBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(111, 137);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 8;
+            this.emailBox.Location = new System.Drawing.Point(111, 114);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(100, 20);
+            this.emailBox.TabIndex = 9;
+            this.emailBox.TextChanged += new System.EventHandler(this.emailBox_TextChanged);
             // 
-            // textBox4
+            // cardBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(111, 174);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 9;
+            this.cardBox.Location = new System.Drawing.Point(111, 184);
+            this.cardBox.Name = "cardBox";
+            this.cardBox.Size = new System.Drawing.Size(100, 20);
+            this.cardBox.TabIndex = 11;
+            this.cardBox.TextChanged += new System.EventHandler(this.cardBox_TextChanged);
             // 
-            // textBox5
+            // dbInput
             // 
-            this.textBox5.Location = new System.Drawing.Point(111, 102);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 10;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(111, 215);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 11;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(136, 364);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "Register";
-            this.button1.UseVisualStyleBackColor = true;
+            this.dbInput.Location = new System.Drawing.Point(262, 15);
+            this.dbInput.Name = "dbInput";
+            this.dbInput.Size = new System.Drawing.Size(75, 23);
+            this.dbInput.TabIndex = 12;
+            this.dbInput.Text = "Register";
+            this.dbInput.UseVisualStyleBackColor = true;
+            this.dbInput.Click += new System.EventHandler(this.dbInput_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(111, 393);
+            this.button2.Location = new System.Drawing.Point(236, 46);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 24);
+            this.button2.Size = new System.Drawing.Size(128, 24);
             this.button2.TabIndex = 13;
             this.button2.Text = "Make an appointment";
             this.button2.UseVisualStyleBackColor = true;
@@ -163,40 +135,64 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(111, 257);
+            this.comboBox1.Location = new System.Drawing.Point(243, 184);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 14;
             // 
-            // label7
+            // lastNameBox
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 301);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(139, 13);
-            this.label7.TabIndex = 15;
-            this.label7.Text = "fields with * must be entered";
+            this.lastNameBox.Location = new System.Drawing.Point(111, 46);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameBox.TabIndex = 16;
+            this.lastNameBox.TextChanged += new System.EventHandler(this.lastNameBox_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Last Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Phone Number";
+            // 
+            // phoneNumBox
+            // 
+            this.phoneNumBox.Location = new System.Drawing.Point(111, 149);
+            this.phoneNumBox.Name = "phoneNumBox";
+            this.phoneNumBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneNumBox.TabIndex = 19;
+            this.phoneNumBox.TextChanged += new System.EventHandler(this.phoneNumBox_TextChanged);
             // 
             // Registration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 444);
-            this.Controls.Add(this.label7);
+            this.ClientSize = new System.Drawing.Size(376, 221);
+            this.Controls.Add(this.phoneNumBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lastNameBox);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dbInput);
+            this.Controls.Add(this.cardBox);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.plateBox);
+            this.Controls.Add(this.nameBox);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Registration";
             this.Text = "Registration";
@@ -208,20 +204,19 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox plateBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.TextBox cardBox;
+        private System.Windows.Forms.Button dbInput;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox phoneNumBox;
     }
 }
--- Elevator Console/Registration.cs
+++ Elevator Console/Registration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// William Bates
+// COSC 2330-001
+// Final Project
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +11,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
 
 namespace ParkALot
 {
@@ -15,6 +21,93 @@ namespace ParkALot
         public Registration()
         {
             InitializeComponent();
+        }
+
+        private void nameBox_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            var FirstName = new Reservation_App();
+            FirstName.Show();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Membership (FirstName)", connection);
+            cmd.Parameters.AddWithValue("@FirstName", FirstName);
+
+            connection.Open();
+        }
+
+        private void lastNameBox_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            var LastName = new Reservation_App();
+            LastName.Show();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Membership (LastName)", connection);
+            cmd.Parameters.AddWithValue("@LastName", LastName);
+            connection.Open();
+        }
+
+        private void plateBox_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            var LicensePlate = new Reservation_App();
+            LicensePlate.Show();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Membership (LicensePlate)", connection);
+            cmd.Parameters.AddWithValue("@LicensePlate", LicensePlate);
+            connection.Open();
+        }
+
+        private void emailBox_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            var Email = new Reservation_App();
+            Email.Show();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Membership (Email)", connection);
+            cmd.Parameters.AddWithValue("@Email", Email);
+            connection.Open();
+        }
+
+        private void phoneNumBox_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            var PhoneNum = new Reservation_App();
+            PhoneNum.Show();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Membership (PhoneNum)", connection);
+            cmd.Parameters.AddWithValue("@PhoneNum", PhoneNum);
+            connection.Open();
+        }
+
+        private void cardBox_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            var CreditCard = new Reservation_App();
+            CreditCard.Show();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Membership (CreditCard)", connection);
+            cmd.Parameters.AddWithValue("@CreditCard", CreditCard);
+            connection.Open();
+        }
+
+        private void dbInput_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Parameters.AddWithValue("@FirstName", nameBox.Text);
+                cmd.Parameters.AddWithValue("@LastName", lastNameBox.Text);
+                cmd.Parameters.AddWithValue("@LicensePlate", plateBox.Text);
+                cmd.Parameters.AddWithValue("@Email", emailBox.Text);
+                cmd.Parameters.AddWithValue("@PhoneNum", phoneNumBox.Text);
+                cmd.Parameters.AddWithValue("@CreditCard", cardBox.Text);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+
+                connection.Open();
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                string message = "Writing to the database was unsuccessful: \n";
+                message += ex.Message;
+                throw new Exception(message);
+            }
         }
     }
 }
