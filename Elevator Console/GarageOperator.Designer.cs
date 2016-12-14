c3946ffad37a30f77eaf6958004afc7d912ed438
Longoria36 <K0486654@amarillocollege.com>
2016-12-14 13:20:50 -0600

    Instantiated an operator object so that when the buttons are clicked it will call the required methods.

--- Elevator Console/GarageOperator.Designer.cs
+++ Elevator Console/GarageOperator.Designer.cs
@@ -44,42 +44,48 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(276, 37);
+            this.label1.Location = new System.Drawing.Point(368, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(47, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Prices";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(246, 108);
+            this.textBox1.Location = new System.Drawing.Point(328, 133);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(246, 150);
+            this.button1.Location = new System.Drawing.Point(328, 185);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 46);
+            this.button1.Size = new System.Drawing.Size(116, 57);
             this.button1.TabIndex = 2;
             this.button1.Text = "save";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(18, 50);
+            this.label2.Location = new System.Drawing.Point(24, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.Size = new System.Drawing.Size(179, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Enter Customer name or ID";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(34, 150);
+            this.button2.Location = new System.Drawing.Point(45, 185);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 46);
+            this.button2.Size = new System.Drawing.Size(133, 57);
             this.button2.TabIndex = 6;
             this.button2.Text = "Look Up";
             this.button2.UseVisualStyleBackColor = true;
@@ -87,34 +93,38 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(34, 107);
+            this.textBox2.Location = new System.Drawing.Point(45, 132);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(243, 65);
+            this.label3.Location = new System.Drawing.Point(324, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 26);
+            this.label3.Size = new System.Drawing.Size(168, 34);
             this.label3.TabIndex = 16;
             this.label3.Text = "Please enter the price for\r\nparking spaces below. \r\n";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(78, 242);
+            this.label4.Location = new System.Drawing.Point(104, 298);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(330, 13);
+            this.label4.Size = new System.Drawing.Size(433, 17);
             this.label4.TabIndex = 17;
             this.label4.Text = "when buttons below are clicked make a messagebox show the data ";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(47, 271);
+            this.button3.Location = new System.Drawing.Point(63, 334);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(105, 45);
+            this.button3.Size = new System.Drawing.Size(140, 55);
             this.button3.TabIndex = 18;
             this.button3.Text = "Empty Spaces";
             this.button3.UseVisualStyleBackColor = true;
@@ -122,9 +132,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(197, 271);
+            this.button4.Location = new System.Drawing.Point(263, 334);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(98, 45);
+            this.button4.Size = new System.Drawing.Size(131, 55);
             this.button4.TabIndex = 19;
             this.button4.Text = "Reserved \r\nSpaces";
             this.button4.UseVisualStyleBackColor = true;
@@ -132,9 +143,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(336, 271);
+            this.button5.Location = new System.Drawing.Point(448, 334);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(108, 45);
+            this.button5.Size = new System.Drawing.Size(144, 55);
             this.button5.TabIndex = 20;
             this.button5.Text = "Full Spaces";
             this.button5.UseVisualStyleBackColor = true;
@@ -142,9 +154,9 @@
             // 
             // Garage_Operator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 358);
+            this.ClientSize = new System.Drawing.Size(669, 441);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -156,6 +168,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Garage_Operator";
             this.Text = "Garage_Operator";
             this.Load += new System.EventHandler(this.Garage_Opporator_Load);
