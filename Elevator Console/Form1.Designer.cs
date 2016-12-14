ac9299e7e427de2a6b38bcd074e253b753b478b1
monkey <monkey@BB21014>
2016-12-14 16:57:02 -0600

    Removed excess buttons

--- Elevator Console/Form1.Designer.cs
+++ Elevator Console/Form1.Designer.cs
@@ -48,9 +48,6 @@
             this.btnLPlateTrue = new System.Windows.Forms.Button();
             this.btnLPlateFalse = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.lblOutput = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -273,44 +270,12 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click_1);
             // 
-            // lblOutput
-            // 
-            this.lblOutput.AutoSize = true;
-            this.lblOutput.Location = new System.Drawing.Point(293, 78);
-            this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(35, 13);
-            this.lblOutput.TabIndex = 20;
-            this.lblOutput.Text = "label3";
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(48, 234);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 21;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(397, 57);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 22;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(477, 356);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.lblOutput);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.btnLPlateFalse);
             this.Controls.Add(this.btnLPlateTrue);
@@ -359,9 +324,6 @@
         private System.Windows.Forms.Button btnLPlateTrue;
         private System.Windows.Forms.Button btnLPlateFalse;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label lblOutput;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button6;
     }
 }
 
